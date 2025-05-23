78e7db3819e350d9bd19632c95aa14889422ed36
jonat@Jonas-PC <jonathasguilherme40@gmail.com>
2025-05-23 10:53:11 -0300

    Mudanças visuais

--- script.js
+++ script.js
@@ -419,7 +419,7 @@ function renderIntro() {
 
 
       <div style="margin-top: 20px; font-size: 13px; color: #444;">
-        <img src="avatar.png" alt="Instrutor Jonas" style="width: 60px; border-radius: 50%;">
+        <img src="jonas.png" alt="Instrutor Jonas" style="width: 60px; border-radius: 50%;">
         <p><em>“Sou o Jonas, Instrutor de Trânsito desde 2020. Já ajudei mais de 2.000 alunos a passar no Detran!”</em></p>
       </div>
 
